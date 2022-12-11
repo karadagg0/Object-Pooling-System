3f30c69915a2863c07f01ce77bd29d81ec15fefb
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-12-11 16:38:24 +0300

    Add files via upload

---
+++ Object-Pooling-Sytsem/Object-Pooling-Sytsem/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject hedef;
+    public GameObject PrefabKarakter;
+    public GameObject SpawnPoint;
+    public GameObject VarisNoktasi;
+    public List<GameObject> characters;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            foreach (var item in characters)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    item.transform.position = SpawnPoint.transform.position;
+                    item.SetActive(true);
+                    break;
+                }
+            }
+        }
+    }
+}
+}
